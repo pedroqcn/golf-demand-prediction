e9108c3dfbeb9e28e2d2739e21fcc7fe2595f328
pedronogueira <pnogueira@wpi.edu>
2026-04-19 01:19:19 -0400

    linear regression model 2

---
+++ data_prep.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import numpy as np
+
+def load_and_clean():
+    """
+    Loads the data and cleans certain parameters for the model to work from
+
+    Returns:
+        The clean DataFrame
+    """
+
+    df = pd.read_csv("golf_dataset/golf_dataset_wide_format.csv")
+
+    # drop play, date, and season
+    df = df.drop(columns=df.filter(like="Play").columns)
+    df = df.drop(columns=["Season"])
+
+    # cyclic encoding for Weekday
+    df["Weekday_sin"] = np.sin(2 * np.pi * df["Weekday"] / 7)
+    df["Weekday_cos"] = np.cos(2 * np.pi * df["Weekday"] / 7)
+
+    df = df.drop(columns=["Weekday"]) # the sine and cosine pair replaces the weekday column
+
+    # cylic encoding for Months
+    month_map = {
+        "Jan": 0, "Feb": 1, "Mar": 2, "Apr": 3,
+        "May": 4, "Jun": 5, "Jul": 6, "Aug": 7,
+        "Sep": 8, "Oct": 9, "Nov": 10, "Dec": 11
+    }
+    df["Month_num"] = df["Month"].map(month_map)
+
+    df["Month_sin"] = np.sin(2 * np.pi * df["Month_num"] / 12)
+    df["Month_cos"] = np.cos(2 * np.pi * df["Month_num"] / 12)
+
+    df = df.drop(columns=["Month", "Month_num"])
+
+    # one-hot encode Outlook
+    df = pd.get_dummies(df, columns=["Outlook"], dtype=int)
+
+    return df
+
+if __name__ == "__main__":
+    df = load_and_clean()
+    print(df.columns.tolist())
+    print(df.shape)
---
+++ model_linear.py
@@ -0,0 +1,36 @@
+from data_prep import load_and_clean
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score, mean_squared_error
+
+df = load_and_clean()
+
+# separate features (X) from target (Y)
+y = df["Crowdedness"]
+X = df.drop(columns=["Crowdedness"])
+
+# split train and test sets chronologically
+# more efficient, since we're learning from past data and predicting future demand
+# -
+# train on 2021-2022, test on 2023
+train = df[df["Date"] < "2023-01-01"]
+test = df[df["Date"] >= "2023-01-01"]
+
+X_train = train.drop(columns=["Crowdedness", "Date"])
+y_train = train["Crowdedness"]
+
+X_test = test.drop(columns=["Crowdedness", "Date"])
+y_test = test["Crowdedness"]
+
+# train the model
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+# predictions
+y_train_pred = model.predict(X_train)
+y_test_pred = model.predict(X_test)
+
+# evaluation
+print(f"Train R^2: {r2_score(y_train, y_train_pred):.4f}")
+print(f"Test R^2: {r2_score(y_test, y_test_pred):.4f}")
+print(f"Test RMSE: {mean_squared_error(y_test, y_test_pred) ** 0.5:.4f}")
