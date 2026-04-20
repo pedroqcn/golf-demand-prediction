5f278b38709f1f5af00f996d35af390b84f1be33
pedronogueira <pnogueira@wpi.edu>
2026-04-20 12:20:11 -0400

    committing again

--- data_prep.py
+++ data_prep.py
@@ -13,7 +13,7 @@ def load_and_clean():
 
     # drop play, date, and season
     df = df.drop(columns=df.filter(like="Play").columns)
-    df = df.drop(columns=["Month"])
+    #df = df.drop(columns=["Month"])
 
     # cyclic encoding for Weekday
     df["Weekday_sin"] = np.sin(2 * np.pi * df["Weekday"] / 7)
@@ -22,17 +22,17 @@ def load_and_clean():
     df = df.drop(columns=["Weekday"]) # the sine and cosine pair replaces the weekday column
 
     # cylic encoding for Months
-  # month_map = {
- #       "Jan": 0, "Feb": 1, "Mar": 2, "Apr": 3,
-  #      "May": 4, "Jun": 5, "Jul": 6, "Aug": 7,
-   #     "Sep": 8, "Oct": 9, "Nov": 10, "Dec": 11
-  #  }
-  #  df["Month_num"] = df["Month"].map(month_map)
+    month_map = {
+        "Jan": 0, "Feb": 1, "Mar": 2, "Apr": 3,
+        "May": 4, "Jun": 5, "Jul": 6, "Aug": 7,
+        "Sep": 8, "Oct": 9, "Nov": 10, "Dec": 11
+    }
+    df["Month_num"] = df["Month"].map(month_map)
 
-    #df["Month_sin"] = np.sin(2 * np.pi * df["Month_num"] / 12)
-  #  df["Month_cos"] = np.cos(2 * np.pi * df["Month_num"] / 12)
+    df["Month_sin"] = np.sin(2 * np.pi * df["Month_num"] / 12)
+    df["Month_cos"] = np.cos(2 * np.pi * df["Month_num"] / 12)
 
-   # df = df.drop(columns=["Month", "Month_num"])
+    df = df.drop(columns=["Month", "Month_num"])
 
     # one-hot encode Outlook
     df = pd.get_dummies(df, columns=["Outlook", "Season"], dtype=int)
